b1ee81b27c5f0e872ad332cc412893febd241ebd
Luka Gulin <lgulin@phy.hr>
2021-02-02 21:48:41 +0100

    Generalised error

--- fs_overlay/opt/certs_manager/lib/open_ssl.rb
+++ fs_overlay/opt/certs_manager/lib/open_ssl.rb
@@ -84,7 +84,7 @@ module OpenSSL
         payload: { csr: File.new(domain.csr_path, 'rb') }
       )
       raise "Error in api Call" unless response.code == 200
-    rescue RestClient::Exception => e
+    rescue
       puts e
       Nginx.stop
       exit
